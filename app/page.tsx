0aefe7305d8b28ff12095c3028bdbf44f76eaff7
justussundelof <justus@sundelof.se>
2025-12-11 12:59:10 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
-import { createClient } from '@/utils/supabase/server'
+import HeaderNav from "@/components/HeaderNav";
+import ProductsGrid from "@/components/ProductsGrid";
 
-import { cookies } from 'next/headers'
-
-export default async function Page() {
-
-  const cookieStore = await cookies()
-
-  const supabase = createClient(cookieStore)
-
-  const { data: articles } = await supabase.from('todos').select()
-
+export default async function Home() {
   return (
-
-    <ul>
-
-      {articles?.map((article) => (
-
-        <li>{article}</li>
-
-      ))}
-
-    </ul>
-
-  )
-
+    <main className="min-h-screen ">
+      <HeaderNav />
+      <section className="mt-8 h-[25vh] bg-pink-600 flex flex-col items-start justify-center px-6 text-white font-sans text-base space-y-6 "></section>
+      <section className="">
+        <ProductsGrid />
+      </section>
+      <section className="h-screen"></section>
+    </main>
+  );
 }
